7be723d680a200f2fef25698b41ef39b12b5978d
Shinetopia <Shinetopiaofficial@outlook.com>
2025-12-29 18:14:07 +0800

    function-basics

--- script.js
+++ script.js
@@ -20,20 +20,37 @@
 // }
 
 function add7(num) {
-    console.log(num + 7);
+  console.log(num + 7);
 }
 add7(10);
 function multiply(num1, num2) {
-    console.log(num1*num2);
+  console.log(num1 * num2);
 }
-multiply(3,2);
-// function capitalize(string) {
-//     for (let i = 0; i < string.length;) {
-//         if (char)
-//     }
-// }
-// capitalize();
+multiply(3, 2);
 
-textBox.addEventListener("keydown", function (event) {
-  console.log(`You pressed "${event.key}".`);
-});
+function capitalize(string) {
+  uppered = string[0].toUpperCase() + string.slice(1).toLowerCase();
+  console.log(uppered);
+}
+capitalize("abcd");
+
+function lastletter(string) {
+  // console.log(string[string.length - 1]);
+  console.log(string.slice(-1));
+}
+lastletter("abcd");
+
+function isCapital(string) {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == string[i].toUpperCase()) {
+      console.log(string[i]);
+      return;
+    }
+  }
+  console.log(string + " has no capital letters");
+}
+isCapital("a");
+
+// textBox.addEventListener("keydown", function (event) {
+//   console.log(`You pressed "${event.key}".`);
+// });
