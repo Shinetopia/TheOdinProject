74f32bbe597f29aed650ed51b4f0ae0c1753b479
Shinetopia <Shinetopiaofficial@outlook.com>
2026-01-08 21:25:07 +0800

    Started js base functions for calculator

---
+++ Calculator/script.js
@@ -0,0 +1,36 @@
+const num1 = 0;
+const operator = 0;
+const num2 = 0;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+  }
+}
