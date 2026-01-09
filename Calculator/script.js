80ad12757ee3d40cd36091cf69d6dd543075a984
Shinetopia <Shinetopiaofficial@outlook.com>
2026-01-09 20:44:34 +0800

    Implemented calculating operation before moving on to next

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,9 +1,8 @@
-const num1 = 0;
-// const operator = 0;
-const num2 = 0;
+let num1 = "";
+let operator = "";
+let num2 = "";
 let finishOpFlag = false;
-let stack = [];
-const operators = ["add", "subtract", "multiply", "divide"];
+// const operators = ["add", "subtract", "multiply", "divide"];
 const gridContainer = document.querySelector(".grid-container");
 const display = document.querySelector("#display-content");
 
@@ -15,54 +14,50 @@ const display = document.querySelector("#display-content");
 gridContainer.addEventListener("click", (event) => {
   let target = event.target;
   if (!isNaN(target.id)) {
-    stack.push(target.id);
-    console.log(stack);
-    updateDisplay();
+    if (operator === "") {
+      num1 += target.id;
+      updateDisplay(num1);
+    } else {
+      num2 += target.id;
+      updateDisplay(num2);
+    }
   } else if (target.id === "clear") {
-    stack.length = 0;
-    updateDisplay();
+    num1 = "";
+    num2 = "";
+    operator = "";
+    updateDisplay(0);
   } else if (target.classList.contains("operator")) {
+    if (num1 !== "" && num2 !== "" && operator !== "") {
+      console.log("num1: " + num1);
+      console.log("num2: " + num2);
+      let result = operate(Number(num1), Number(num2), operator);
+      updateDisplay(result);
+      num1 = result;
+      num2 = "";
+    }
+    operator = target.id;
     let currentActive = gridContainer.querySelector(".toggle");
+    console.log(operator);
     if (currentActive && currentActive !== target) {
       currentActive.classList.remove("toggle");
     }
-    const operatorMatch = stack.find(
-      (operator) =>
-        operators.includes(operator) && !isNaN(stack[stack.length - 1])
-    );
-    // do previous operation
-    if (operatorMatch) {
-      prepForOperate(operatorMatch, target.id);
-    }
-    // still choosing operator
     target.classList.toggle("toggle");
-    stack.pop();
-    stack.push(operatorMatch);
   }
 });
 
-// combines numbers of either side of first operator
-// oh my coupling & cohesion
-function prepForOperate(operator, nextOperator) {
-  let opIndex = stack.indexOf(operator);
-  let num1 = stack.slice(0, opIndex).join("");
-  let num2 = stack.slice(opIndex + 1).join("");
-  let result = operate(num1, num2, operator);
-  updateDisplay(result, true);
-  stack.length = 0;
-  stack.push(result);
-  stack.push(nextOperator);
+function updateDisplay(num) {
+  display.textContent = num;
 }
 
-function updateDisplay(num, prevOpFlag) {
-  if (stack.length === 0) {
-    display.textContent = "0";
-  } else if (prevOpFlag === true) {
-    display.textContent = num;
-  } else {
-    display.textContent = stack.join("");
-  }
-}
+// function updateDisplay(num, prevOpFlag) {
+//   if (stack.length === 0) {
+//     display.textContent = "0";
+//   } else if (prevOpFlag === true) {
+//     display.textContent = num;
+//   } else {
+//     display.textContent = stack.join("");
+//   }
+// }
 
 function add(a, b) {
   return a + b;
@@ -82,20 +77,15 @@ function divide(a, b) {
 
 function operate(num1, num2, operator) {
   switch (operator) {
-    case "+":
-      add(num1, num2);
-      break;
-    case "-":
-      subtract(num1, num2);
-      break;
-    case "*":
-      multiply(num1, num2);
-      break;
-    case "/":
-      divide(num1, num2);
-      break;
-    case "=":
-      finishOpFlag = true;
-      break;
+    case "add":
+      return add(num1, num2);
+    case "subtract":
+      return subtract(num1, num2);
+    case "multiply":
+      return multiply(num1, num2);
+    case "divide":
+      return divide(num1, num2);
+    case "equal":
+      return (finishOpFlag = true);
   }
 }
