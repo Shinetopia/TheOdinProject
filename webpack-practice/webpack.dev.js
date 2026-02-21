fcf2da648fde4608214f500cd7576e3e37a4ade0
Shinetopia <ShinetopiaOfficial@outlook.com>
2026-02-21 10:24:55 +0800

    Made a common for webpack modes and merging

--- webpack-practice/webpack.dev.js
+++ webpack-practice/webpack.dev.js
@@ -1,7 +1,13 @@
-// // webpack.config.js
-// import path from "node:path";
-// import HtmlWebpackPlugin from "html-webpack-plugin";
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
+export default merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+});
 // export default {
 //   mode: "development",
 //   entry: "./src/index.js",
--- webpack-practice/webpack.prod.js
+++ webpack-practice/webpack.prod.js
@@ -1,6 +1,9 @@
-// // webpack.config.js
-// import path from "node:path";
-// import HtmlWebpackPlugin from "html-webpack-plugin";
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+  mode: "production",
+});
 
 // export default {
 //   mode: "development",
